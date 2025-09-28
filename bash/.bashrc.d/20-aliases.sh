492976efb993861739b1db4d7f470e689b213fd7
Richard Hao <richard@0xdev.dev>
2025-09-28 09:29:44 +0800

    feat: add yazi config and eza aliases
    Adds configuration for the yazi file manager.
    Replaces ls with eza and adds new aliases for it in the bash config.

--- bash/.bashrc.d/20-aliases.sh
+++ bash/.bashrc.d/20-aliases.sh
@@ -1,4 +1,9 @@
-alias ll='ls -alF'
 alias gs='git status -sb'
 alias vim='nvim'
 alias e='${EDITOR}'
+
+
+# Use eza instead of ls
+alias ls="eza --group-directories-first --icons -a"
+alias ll="eza -lh --group-directories-first --icons -a"
+alias la="eza -lha --group-directories-first --icons"
