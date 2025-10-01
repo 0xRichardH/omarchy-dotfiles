0c7cc616d59106bc4d82895dccb0074bdbca5705
Richard Hao <richard@0xdev.dev>
2025-10-02 01:37:10 +0800

    feat(nvim): enable copilot and disable ai_cmp
    Enables copilot and disables the now redundant ai_cmp setting.

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.coding.blink" },
     { import = "lazyvim.plugins.extras.ai.sidekick" },
+    { import = "lazyvim.plugins.extras.ai.copilot" },
     -- import/override with your plugins
     { import = "plugins" },
   },
