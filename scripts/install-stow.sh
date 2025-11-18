536be5a44d311b369b0257011d2d57b8fc9a219f
Richard Hao <richard@0xdev.dev>
2025-11-18 22:25:06 +0800

    feat: install xremap

--- scripts/install-stow.sh
+++ scripts/install-stow.sh
@@ -2,3 +2,12 @@
 
 # Install stow
 sudo pacman -S --noconfirm --needed stow
+
+# Restow the packages
+
+stow --target="$HOME" --adopt nvim
+stow --target="$HOME" --adopt hypr
+stow --target="$HOME" --adopt git
+stow --target="$HOME" --adopt bash
+stow --target="$HOME" --adopt bw
+stow --target="$HOME" --adopt ssh
