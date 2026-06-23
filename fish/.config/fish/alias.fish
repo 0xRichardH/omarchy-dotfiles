3d32ec9e5d6df8bd207b490d859ae3f0357f887d
Richard Hao <richard@0xdev.dev>
2026-06-23 10:01:44 +0800

    refactor(fish): alias x to pi, add oc alias for opencode attach

--- fish/.config/fish/alias.fish
+++ fish/.config/fish/alias.fish
@@ -1,3 +1,4 @@
 alias cat bat
 alias p pi
-alias x "OPENCODE_EXPERIMENTAL=1 OPENCODE_SERVER_PASSWORD=(cat ~/.credentials/opencode_password) opencode attach http://127.0.0.1:4096 --dir ."
+alias x pi
+alias oc "OPENCODE_EXPERIMENTAL=1 OPENCODE_SERVER_PASSWORD=(cat ~/.credentials/opencode_password) opencode attach http://127.0.0.1:4096 --dir ."
